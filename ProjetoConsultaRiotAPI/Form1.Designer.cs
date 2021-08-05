275bf916e0009712c733e5c0c45ce39bd1e57a8e
ZoioHenri <zoiohenri12@gmail.com>
2021-08-05 01:45:24 -0300

    primeiro commit

--- ProjetoConsultaRiotAPI/Form1.Designer.cs
+++ ProjetoConsultaRiotAPI/Form1.Designer.cs
@@ -31,21 +31,24 @@ namespace ProjetoConsultaRiotAPI
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
             this.txtNick = new System.Windows.Forms.TextBox();
             this.btnConsultar = new System.Windows.Forms.Button();
             this.btnCancelar = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtDerrotas = new System.Windows.Forms.TextBox();
+            this.txtWins = new System.Windows.Forms.TextBox();
+            this.txtIconID = new System.Windows.Forms.TextBox();
+            this.txtNivel = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnLimpar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -57,78 +60,6 @@ namespace ProjetoConsultaRiotAPI
             this.label1.Size = new System.Drawing.Size(114, 18);
             this.label1.TabIndex = 0;
             this.label1.Text = "Insira seu nick:";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(20, 87);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(49, 18);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Nivel:";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(20, 125);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(48, 18);
-            this.label3.TabIndex = 1;
-            this.label3.Text = "Rank:";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(22, 163);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(46, 18);
-            this.label4.TabIndex = 1;
-            this.label4.Text = "Wins:";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(24, 204);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(74, 18);
-            this.label5.TabIndex = 2;
-            this.label5.Text = "Derrotas:";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(76, 87);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(99, 25);
-            this.textBox1.TabIndex = 3;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(75, 125);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 25);
-            this.textBox2.TabIndex = 3;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(75, 160);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(59, 25);
-            this.textBox3.TabIndex = 3;
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(105, 201);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(53, 25);
-            this.textBox4.TabIndex = 3;
             // 
             // txtNick
             // 
@@ -155,6 +86,7 @@ namespace ProjetoConsultaRiotAPI
             this.btnCancelar.TabIndex = 5;
             this.btnCancelar.Text = "Cancelar";
             this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
             // 
             // pictureBox1
             // 
@@ -174,32 +106,121 @@ namespace ProjetoConsultaRiotAPI
             this.pictureBox2.TabIndex = 7;
             this.pictureBox2.TabStop = false;
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtDerrotas);
+            this.groupBox1.Controls.Add(this.txtWins);
+            this.groupBox1.Controls.Add(this.txtIconID);
+            this.groupBox1.Controls.Add(this.txtNivel);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 71);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 175);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Resultado";
+            // 
+            // txtDerrotas
+            // 
+            this.txtDerrotas.Location = new System.Drawing.Point(82, 129);
+            this.txtDerrotas.Name = "txtDerrotas";
+            this.txtDerrotas.Size = new System.Drawing.Size(100, 25);
+            this.txtDerrotas.TabIndex = 10;
+            // 
+            // txtWins
+            // 
+            this.txtWins.Location = new System.Drawing.Point(61, 93);
+            this.txtWins.Name = "txtWins";
+            this.txtWins.Size = new System.Drawing.Size(100, 25);
+            this.txtWins.TabIndex = 10;
+            // 
+            // txtIconID
+            // 
+            this.txtIconID.Location = new System.Drawing.Point(66, 53);
+            this.txtIconID.Name = "txtIconID";
+            this.txtIconID.Size = new System.Drawing.Size(100, 25);
+            this.txtIconID.TabIndex = 10;
+            // 
+            // txtNivel
+            // 
+            this.txtNivel.Location = new System.Drawing.Point(61, 22);
+            this.txtNivel.Name = "txtNivel";
+            this.txtNivel.Size = new System.Drawing.Size(100, 25);
+            this.txtNivel.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 132);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 18);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Derrotas:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 18);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Wins:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 56);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 18);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "IconID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 18);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Nivel:";
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(94, 253);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(84, 30);
+            this.btnLimpar.TabIndex = 9;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(396, 304);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btnCancelar);
             this.Controls.Add(this.btnConsultar);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.txtNick);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Consulta";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -208,19 +229,21 @@ namespace ProjetoConsultaRiotAPI
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.TextBox txtNick;
         private System.Windows.Forms.Button btnConsultar;
         private System.Windows.Forms.Button btnCancelar;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDerrotas;
+        private System.Windows.Forms.TextBox txtWins;
+        private System.Windows.Forms.TextBox txtIconID;
+        private System.Windows.Forms.TextBox txtNivel;
+        private System.Windows.Forms.Button btnLimpar;
     }
 }
 
--- ProjetoConsultaRiotAPI/Form1.cs
+++ ProjetoConsultaRiotAPI/Form1.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.Http;
 using System.Windows.Forms;
+using Newtonsoft.Json;
 
 namespace ProjetoConsultaRiotAPI
 {
@@ -17,21 +12,74 @@ namespace ProjetoConsultaRiotAPI
             InitializeComponent();
         }
 
-        public class Jogador
+        public class JogadorResult
         {
+            [JsonProperty(PropertyName = "id")]
             public string id { get; set; }
+
+            [JsonProperty(PropertyName = "accountId")]
             public string accountId { get; set; }
+            
+            [JsonProperty(PropertyName = "puuid")]
             public string puuid { get; set; }
+
+            [JsonProperty(PropertyName = "name")]
             public string name { get; set; }
+
+            [JsonProperty(PropertyName = "profileIconId")]
             public int profileIconId { get; set; }
+
+            [JsonProperty(PropertyName = "revisionDate")]
             public long revisionDate { get; set; }
+
+            [JsonProperty(PropertyName = "summonerLevel")]
             public int summonerLevel { get; set; }
+            
         }
 
         private void btnConsultar_Click(object sender, EventArgs e)
         {
-            if (string.IsNullOrEmpty(txtNick.Text)) ;
-            MessageBox.Show("Digite seu nick","Aviso!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            if  (string.IsNullOrWhiteSpace(txtNick.Text))
+            MessageBox.Show("Digite seu nick!", "Aviso!",MessageBoxButtons.OK,MessageBoxIcon.Exclamation);
+            else
+            {
+                string ApiURL = string.Format("https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{0}?api_key=RGAPI-7f721438-c2c5-4556-b800-d01b20fe7630", txtNick.Text.Trim());
+                try
+                {
+                    using (HttpClient client = new HttpClient()) 
+                    {
+                        var response = client.GetAsync(ApiURL).Result;
+                        if (response.IsSuccessStatusCode)
+                        {
+                            var result = response.Content.ReadAsStringAsync().Result;
+                            JogadorResult res = JsonConvert.DeserializeObject<JogadorResult>(result);
+
+                            txtNivel.Text = res.summonerLevel.ToString();
+                            txtIconID.Text = res.profileIconId.ToString();
+
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, this.Text, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                  
+                }
+            }
+
+        }
+        
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtNick.Text = string.Empty;
+            txtIconID.Text = string.Empty;
+            txtNivel.Text = string.Empty;
         }
     }
 }
